bfe2480bdbd5787d8a8daa455be0ae523d0f6294
AndrewStattADA <andrew.statt@ada.ac.uk>
2026-03-24 18:06:23 +0000

    spelling mistake corrected

--- InvManSys-Backend/backend_core/settings.py
+++ InvManSys-Backend/backend_core/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "corsheaders.middlewear.CorsMiddlewear",
+    "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -119,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+CORS_ORIGIN_WHITELIST =[
+    "https://localhost:3000"
+]
+
+CORS_ALLOWED_ORIGIN_REGEXES=[
+    r"^https:\/\/.*-3000\.app\.github\dev$",
+]
