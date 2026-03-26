1b4280a48bec7aa8b029e2300de98b176025082a
AndrewStattADA <andrew.statt@ada.ac.uk>
2026-03-26 17:50:17 +0000

    adding profile to the imports and register so I can see it in the backend

--- InvManSys-Backend/inventory_api/admin.py
+++ InvManSys-Backend/inventory_api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import InventoryItem, Category, StockLog
+from .models import InventoryItem, Category, StockLog, Profile
 
 admin.site.register(InventoryItem)
 admin.site.register(Category)
 admin.site.register(StockLog)
+admin.site.register(Profile)
