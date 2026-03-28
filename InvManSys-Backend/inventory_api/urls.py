db618da22729ae9de08daf7abb396a2917d9e50a
AndrewStattADA <andrew.statt@ada.ac.uk>
2026-03-28 14:08:09 +0000

    Routes and imports updated + migrations

--- InvManSys-Backend/inventory_api/urls.py
+++ InvManSys-Backend/inventory_api/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import InventoryItemViewSet, CategoryViewSet, StockLogViewSet, RegisterView, UserManagementViewSet
+from . import views
 
 # The router automatically creates the standard GET, POST, PUT, DELETE routes
 router = DefaultRouter()
 router.register(r'items', InventoryItemViewSet, basename='inventoryitem')
 router.register(r'categories', CategoryViewSet, basename='category')
-router.register(r'logs', StockLogViewSet, basename='stocklog')
+router.register(r'stock-logs', views.StockLogViewSet, basename='stocklog')
 router.register(r'users', UserManagementViewSet, basename='users')
+router.register(r'user-action-logs', views.UserActionLogViewSet, basename='useractionlog')
 
 urlpatterns = [
     path('', include(router.urls)),
