b8446858318b916fe38ac19beff72d02948c8f8e
AndrewStattADA <andrew.statt@ada.ac.uk>
2026-04-20 11:21:59 +0000

    new paths and imports added

--- InvManSys-Backend/inventory_api/urls.py
+++ InvManSys-Backend/inventory_api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import InventoryItemViewSet, CategoryViewSet, StockLogViewSet, RegisterView, UserManagementViewSet, request_password_reset, reset_password_confirm
+from .views import InventoryItemViewSet, CategoryViewSet, StockLogViewSet, RegisterView, UserManagementViewSet, UserActionLogViewSet, request_password_reset, reset_password_confirm
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
 
-# The router automatically creates the standard GET, POST, PUT, DELETE routes
 router = DefaultRouter()
 router.register(r'items', InventoryItemViewSet, basename='inventoryitem')
 router.register(r'categories', CategoryViewSet, basename='category')
@@ -13,6 +13,9 @@ router.register(r'user-action-logs', views.UserActionLogViewSet, basename='usera
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
     path('register/', RegisterView.as_view(), name='auth_register'),
     path('password-reset/', request_password_reset, name='password_reset'),
     path('password-reset-confirm/', reset_password_confirm, name='password_reset_confirm'),
